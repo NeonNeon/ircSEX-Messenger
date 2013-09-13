0f2b11e6232a46766c080a7f01099e9dc229b449
Wilhelm Hedman <hedmanw@student.chalmers.se>
2013-09-13 17:53:39 +0200

    Refactored out server connect dialog to fragment

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/ServerListActivity.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/ServerListActivity.java
@@ -1,18 +1,19 @@
 package se.chalmers.dat255.ircsex.ui;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import se.chalmers.dat255.ircsex.R;
 
 /**
  * Created by wilhelm on 9/13/13.
  */
-public class ServerListActivity extends Activity {
+public class ServerListActivity extends FragmentActivity implements ServerConnectDialogFragment.DialogListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,22 +21,21 @@ public class ServerListActivity extends Activity {
     }
 
     public void popupServerConnect(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.dialog_serverconnect, null))
-                .setTitle("Connect to server")
-                .setPositiveButton(R.string.connect, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        // TODO: Connect to server through backend
-                    }
-                })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
+        DialogFragment fragment = new ServerConnectDialogFragment();
+        fragment.show(getSupportFragmentManager(), "serverconnect");
+    }
 
-                    }
-                });
-        builder.create().show();
+    @Override
+    public void onDialogAccept(DialogFragment dialogFragment) {
+        Dialog dialog = dialogFragment.getDialog();
+        String server = ((TextView)dialog.findViewById(R.id.dialog_serverconnect_server)).getText().toString();
+        String port = ((TextView)dialog.findViewById(R.id.dialog_serverconnect_port)).getText().toString();
+        String nickname = ((TextView)dialog.findViewById(R.id.dialog_serverconnect_nickname)).getText().toString();
+        // TODO: Do fun stuff with this data
+    }
+
+    @Override
+    public void onDialogCancel(DialogFragment dialog) {
+        dialog.getDialog().cancel();
     }
 }
