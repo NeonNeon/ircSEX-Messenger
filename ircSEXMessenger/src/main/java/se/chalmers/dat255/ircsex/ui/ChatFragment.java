1e4160fdcdc7116439f3af6206c5ec9537e0d383
Wilhelm Hedman <w.hedman@gmail.com>
2013-09-17 15:16:51 +0200

    Made UI  changes for serverconnect

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/ChatFragment.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/ChatFragment.java
@@ -21,11 +21,11 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
         int i = getArguments().getInt(ARG_PLANET_NUMBER);
-        String planet = getResources().getStringArray(R.array.planets_array)[i];
+//        String planet = getResources().getStringArray(R.array.planets_array)[i];
 
-        ((TextView) rootView.findViewById(R.id.textView)).setText(planet);
-        getActivity().setTitle(planet);
-        getActivity().getActionBar().setSubtitle("irc." + planet.toLowerCase() + ".com");
+        ((TextView) rootView.findViewById(R.id.textView)).setText("hest");
+        getActivity().setTitle("hest");
+        getActivity().getActionBar().setSubtitle("irc.hest" + ".com");
         return rootView;
     }
 }
