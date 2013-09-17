9343c29c20c4f880d483d0535c6d10ce17791d6f
rascal <oskar@oskarnyberg.com>
2013-09-17 14:54:38 +0200

    Created constructor in IrcChannel

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/IrcChannel.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/IrcChannel.java
@@ -1,7 +1,20 @@
 package se.chalmers.dat255.ircsex.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import se.chalmers.dat255.ircsex.irc.IrcProtocolAdapter;
+
 /**
  * Created by Oskar on 2013-09-17.
  */
 public class IrcChannel {
+
+    private final String channelName;
+    private final List<String> users;
+
+    public IrcChannel(String channelName, IrcProtocolAdapter protocol) {
+        this.channelName = channelName;
+        users = new ArrayList<String>();
+    }
 }
