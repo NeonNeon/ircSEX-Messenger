11221919a33ea55fafdd4e9e38976f0537945018
rascal <oskar@oskarnyberg.com>
2013-09-17 14:08:47 +0200

    Added handeling of servers in Session

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/Session.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/Session.java
@@ -1,7 +1,23 @@
 package se.chalmers.dat255.ircsex.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Oskar on 2013-09-17.
  */
 public class Session {
+    private List<IrcServer> servers;
+
+    public Session() {
+        servers = new ArrayList<IrcServer>();
+    }
+
+    public void addServer(String server, int port) {
+        servers.add(new IrcServer(server, port));
+    }
+
+    public void removeServer(IrcServer server) {
+        servers.remove(server);
+    }
 }
