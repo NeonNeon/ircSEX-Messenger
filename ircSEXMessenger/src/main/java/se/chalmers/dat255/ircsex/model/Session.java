f18a7a700f730efdd185eefea049e972eb40af87
rascal <oskar@oskarnyberg.com>
2013-09-17 14:52:08 +0200

    Added nick to constructor

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/Session.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/Session.java
@@ -1,23 +1,23 @@
 package se.chalmers.dat255.ircsex.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Oskar on 2013-09-17.
  */
 public class Session {
-    private List<IrcServer> servers;
+    private Map<String, IrcServer> servers;
 
     public Session() {
-        servers = new ArrayList<IrcServer>();
+        servers = new HashMap<String, IrcServer>();
     }
 
-    public void addServer(String server, int port) {
-        servers.add(new IrcServer(server, port));
+    public void addServer(String server, int port, String nick) {
+        servers.put(server, new IrcServer(server, port, nick));
     }
 
-    public void removeServer(IrcServer server) {
-        servers.remove(server);
+    public void removeServer(String host) {
+        servers.remove(host);
     }
 }
