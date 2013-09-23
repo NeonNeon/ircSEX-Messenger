0108a9c221a76219c5b0b18d26d6835a21393995
Wilhelm Hedman <hedmanw@student.chalmers.se>
2013-09-23 19:17:47 +0200

    Server list header gives visual feedback when it's disconnected

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/view/CollapsableListHeader.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/view/CollapsableListHeader.java
@@ -1,7 +1,12 @@
 package se.chalmers.dat255.ircsex.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -9,9 +14,21 @@ import android.widget.TextView;
  * Created by Wilhelm on 2013-09-18.
  */
 public class CollapsableListHeader extends TextView {
+    private String header;
 
     public CollapsableListHeader(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setText(getText().toString().toUpperCase());
+        header = getText().toString().toUpperCase();
+        setText(header);
+    }
+
+    public void setDisabled() {
+        Spannable wordToSpan = new SpannableString(header + " (DISCONNECTED)");
+        wordToSpan.setSpan(new ForegroundColorSpan(0xFFFF4444), header.length(), wordToSpan.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        setText(wordToSpan);
+    }
+
+    public void setEnabled() {
+        setText(header);
     }
 }
