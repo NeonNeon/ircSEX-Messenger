d7a8db6cb467fc9bb36d5a501d4773fc3b9d2799
Joel Torstensson <oed3933@gmail.com>
2013-09-24 10:30:58 +0200

    Added javadoc for IPL.

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/IrcProtocolListener.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/IrcProtocolListener.java
@@ -7,19 +7,45 @@ package se.chalmers.dat255.ircsex.irc;
 public interface IrcProtocolListener {
 
 
+    /**
+     * Called when connection to the server has been established.
+     */
     public void serverConnected();
 
+    /**
+     * Called when your are registered on the server.
+     */
     public void serverRegistered();
 
+    /**
+     * Called when you join a channel.
+     * @param channelName - the name of the channel you joined
+     */
     public void joinedChannel(String channelName);
 
+    /**
+     * Called when you part a channel.
+     * @param channelName - - the name of the channel you parted
+     */
     public void partedChannel(String channelName);
 
+    /**
+     * Called when a user changed nick.
+     * @param oldNick - the nick that the user had until now
+     * @param newNick - the new nick of the user
+     */
     public void nickChanged(String oldNick, String newNick);
 
     // ERRORS
 
+    /**
+     * Something went wrong when you tried to change nick.
+     */
     public void nickChangeError();
 
+    /**
+     * The server was disconnected.
+     * To reconnect get a new IPA.
+     */
     public void ServerDisconnected();
 }
