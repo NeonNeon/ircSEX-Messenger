bc85cf380e098705cdc73e4ce5da28cf6b7d388e
Joel Torstensson <oed3933@gmail.com>
2013-09-24 10:40:35 +0200

    Added callbacks for listing users in IPL.

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/IrcProtocolListener.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/IrcProtocolListener.java
@@ -1,5 +1,7 @@
 package se.chalmers.dat255.ircsex.irc;
 
+import java.util.List;
+
 /**
  * Created by oed on 9/23/13.
  * An interface that listens to events that are relevant to the IRC Server.
@@ -16,6 +18,27 @@ public interface IrcProtocolListener {
     public void partedChannel(String channelName);
 
     public void nickChanged(String oldNick, String newNick);
+
+    /**
+     * This method gives you info about which users are in a specific channel.
+     * @param channelName
+     * @param users
+     */
+    public void usersInChannel(String channelName, List<String> users);
+
+    /**
+     * This method is called when a user joins a channel.
+     * @param channelName
+     * @param nick
+     */
+    public void userJoined(String channelName, String nick);
+
+    /**
+     * This method is called when a user parts a channel.
+     * @param channelName
+     * @param nick
+     */
+    public void userParted(String channelName, String nick);
 
     // ERRORS
 
