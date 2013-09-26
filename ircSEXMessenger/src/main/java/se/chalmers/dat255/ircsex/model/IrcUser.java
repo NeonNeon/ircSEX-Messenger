946922d20d53608d1545b44f15d482a7c484daec
rascal <oskar@oskarnyberg.com>
2013-09-26 14:36:45 +0200

    Added half op in IrcUser

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/IrcUser.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/model/IrcUser.java
@@ -7,21 +7,24 @@ public class IrcUser implements Comparable<IrcUser> {
 
     private String nick;
     private char status;
-    private boolean op;
-    private boolean voice;
     private boolean owner;
+    private boolean op;
+    private boolean halfOp;
+    private boolean voice;
 
-    public static final char OP = '@';
-    public static final char VOICE = '+';
     public static final char OWNER = '~';
+    public static final char OP = '@';
+    public static final char HALF_OP = '%';
+    public static final char VOICE = '+';
     public static final char NO_STATUS = (char) -1;
 
     public IrcUser(String user, char status) {
         this.nick = user;
         this.status = status;
+        owner = status == OWNER;
         op = status == OP;
         voice = status == VOICE;
-        owner = status == OWNER;
+        halfOp = status == HALF_OP;
     }
 
     /**
@@ -33,16 +36,20 @@ public class IrcUser implements Comparable<IrcUser> {
         return nick;
     }
 
+    public boolean isOwner() {
+        return owner;
+    }
+
     public boolean isOp() {
         return op;
     }
 
-    public boolean isVoice() {
-        return voice;
+    public boolean isHalfOp() {
+        return halfOp;
     }
 
-    public boolean isOwner() {
-        return owner;
+    public boolean isVoice() {
+        return voice;
     }
 
     public void changeNick(String nick) {
@@ -51,7 +58,7 @@ public class IrcUser implements Comparable<IrcUser> {
 
     @Override
     public String toString() {
-        return (status == NO_STATUS ? " " : status) + nick;
+        return (status == NO_STATUS ? "  " : status) + nick;
     }
 
     @Override
@@ -60,6 +67,8 @@ public class IrcUser implements Comparable<IrcUser> {
             return Boolean.valueOf(ircUser.isOwner()).compareTo(Boolean.valueOf(owner));
         } else if (op != ircUser.isOp()) {
             return Boolean.valueOf(ircUser.isOp()).compareTo(Boolean.valueOf(op));
+        } else if (halfOp != ircUser.isHalfOp()) {
+            return Boolean.valueOf(ircUser.isHalfOp()).compareTo(Boolean.valueOf(halfOp));
         } else if (voice != ircUser.isVoice()) {
             return Boolean.valueOf(ircUser.isVoice()).compareTo(Boolean.valueOf(voice));
         } else {
@@ -75,7 +84,7 @@ public class IrcUser implements Comparable<IrcUser> {
      */
     public static char extractUserStatus(String username) {
         char first = username.charAt(0);
-        if (first == OP || first == VOICE || first == OWNER) {
+        if (first == OP || first == VOICE || first == OWNER || first == HALF_OP) {
             return first;
         } else {
             return NO_STATUS;
