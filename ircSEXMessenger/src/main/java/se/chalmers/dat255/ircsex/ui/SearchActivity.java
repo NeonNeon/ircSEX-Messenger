79a88de801e4b4b97f07caf2cbf9949f8232d9e9
rascal <oskar@oskarnyberg.com>
2013-10-07 11:02:43 +0200

    Renamed search_list_item

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/SearchActivity.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/SearchActivity.java
@@ -71,7 +71,7 @@ public class SearchActivity extends ListActivity {
         adapter = new SimpleAdapter(
                 this,
                 result,
-                R.layout.search_list_item,
+                R.layout.channel_search_list_item,
                 new String[]{TEXT1, TEXT2, TEXT3},
                 new int[]{R.id.text1, R.id.text2, R.id.text3});
         setListAdapter(adapter);
