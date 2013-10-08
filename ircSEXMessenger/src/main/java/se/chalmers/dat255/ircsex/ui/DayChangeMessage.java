eb9c8c8317c553f69c76731ebc1664f847170f52
Johan Magnusson <jcmmagnusson@gmail.com>
2013-10-08 14:41:06 +0200

    DayChangeMessage extends InfoMessage

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/DayChangeMessage.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/ui/DayChangeMessage.java
@@ -1,44 +1,14 @@
 package se.chalmers.dat255.ircsex.ui;
 
-import android.graphics.Color;
-import android.graphics.Rect;
-import android.view.Gravity;
-
 import se.chalmers.dat255.ircsex.R;
 
 /**
  * Created by Johan on 2013-10-01.
  */
-public class DayChangeMessage implements ChannelItem {
-    private final String message;
+public class DayChangeMessage extends InfoMessage {
 
     public DayChangeMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
-    @Override
-    public int getGravity() {
-        return Gravity.CENTER;
-    }
-
-    @Override
-    public Rect getPadding() {
-        return new Rect(40, 15, 40, 20);
-    }
-
-    @Override
-    public int getColor() {
-        return Color.rgb(240, 240, 240);
-    }
-
-    @Override
-    public int getNinePatchID() {
-        return R.drawable.message_item;
+        super(message);
     }
 
     @Override
