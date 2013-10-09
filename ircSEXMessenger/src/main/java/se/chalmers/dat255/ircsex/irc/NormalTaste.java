9fbc0a80c0e3d14227c4565e1686cd0187a7a93d
Joel Torstensson <oed3933@gmail.com>
2013-10-09 18:21:29 +0200

    Implemented NormalTaste.

--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/NormalTaste.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/NormalTaste.java
@@ -2,6 +2,10 @@ package se.chalmers.dat255.ircsex.irc;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 /**
  * Created by oed on 10/9/13.
@@ -10,24 +14,44 @@ public class NormalTaste implements Taste {
 
     private String host;
     private int port;
+    private boolean buffersCreated;
+
+    private Socket socket;
+    private BufferedReader input;
+    private BufferedWriter output;
 
     public NormalTaste(String host, int port) {
         this.host = host;
         this.port = port;
+        buffersCreated = false;
     }
 
     @Override
-    public BufferedReader getInput() {
-        return null;
+    public BufferedReader getInput() throws IOException {
+        if (!buffersCreated) {
+            createBuffers();
+        }
+        return input;
     }
 
     @Override
-    public BufferedWriter getOutput() {
-        return null;
+    public BufferedWriter getOutput() throws IOException {
+        if (!buffersCreated) {
+            createBuffers();
+        }
+        return output;
     }
 
     @Override
-    public void close() {
+    public void close() throws IOException {
+        input.close();
+        output.close();
+        socket.close();
+    }
 
+    private void createBuffers() throws IOException {
+        socket = new Socket(host, port);
+        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
 }
--- ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/Taste.java
+++ ircSEXMessenger/src/main/java/se/chalmers/dat255/ircsex/irc/Taste.java
@@ -2,16 +2,17 @@ package se.chalmers.dat255.ircsex.irc;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 
 /**
  * Created by oed on 10/9/13.
  */
 public interface Taste {
 
-    public BufferedReader getInput();
+    public BufferedReader getInput() throws IOException;
 
-    public BufferedWriter getOutput();
+    public BufferedWriter getOutput() throws IOException;
 
-    public void close();
+    public void close() throws IOException;
 
 }
